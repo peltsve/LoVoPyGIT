73eea15edd7a8e84540e17848a9a70c3cf4a6b09
peltsve <81988314+peltsve@users.noreply.github.com>
2023-11-12 23:13:15 +0300

    Сделал пустышки-функции для будщей отрисовки фона, дома, солнца

--- LoVoExplorer.py
+++ LoVoExplorer.py
@@ -9,10 +9,24 @@ def draw_picture(screen, x, y, width, height):
     :param y: координата по вертикали
     :param width: ширина  прямоугольника
     :param height: высота прямоугольника
-
     """
     print("Типа рисую картинку", x, y, width, height)
+    draw_background(screen, x, y, width, height)
+    house_x = x + width // 2
+    house_y = y + 3 * height // 4
+    house_height = min(height, width) * 2 // 3
+    house_width = int(house_height*1.5)
+    draw_house(screen, house_x, house_y, house_width, house_height)
+    sun_radius = min(height, width)//10
+    draw_sun(screen, x, y, sun_radius)
     pass
+def draw_sun(screen, x, y, sun_radius):
+    print("Типа рисую солнышко", x, y, sun_radius)
+def draw_house(screen, house_x, house_y, house_width, house_height):
+    print("Типа рисую домик: ", house_x, house_y, house_width, house_height)
+def draw_background(screen, x, y, width, height):
+    print("Типа рисую фон ", x, y, width, height)
+
 FPS = 30
 width, height = screen_size = (400, 400)
 screen = pygame.display.set_mode(screen_size)
