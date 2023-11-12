f694e66480422f0a7ecb324c0982e71f8e6c9e84
peltsve <81988314+peltsve@users.noreply.github.com>
2023-11-12 21:45:58 +0300

    Drowning The House First Screen

--- LoVoExplorer.py
+++ LoVoExplorer.py
@@ -4,8 +4,22 @@ from pygame.draw import *
 pygame.init()
 
 FPS = 30
-screen = pygame.display.set_mode((400, 400))
+width, height = screen_size = (400, 400)
+screen = pygame.display.set_mode(screen_size)
 
+pygame.display.update()
+
+work_flag = True
+while work_flag:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            work_flag = False
+
+
+print("Программа благополцчно завершена!")
+"""
+# старый домик
 rect(screen, (255, 0, 255), (100, 100, 200, 200))
 rect(screen, (0, 0, 255), (100, 100, 200, 200), 5)
 polygon(screen, (255, 255, 0), [(100,100), (200,50),
@@ -26,3 +40,4 @@ while not finished:
             finished = True
 
 pygame.quit()
+"""
